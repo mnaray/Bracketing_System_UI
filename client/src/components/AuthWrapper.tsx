2b7845000fcb20b0c289adda259deb3a64d37e65
n¿co <89131248+h0peRL@users.noreply.github.com>
2024-04-04 10:51:37 +0200

    add AuthWrapper to project

---
+++ client/src/components/AuthWrapper.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState, ReactNode, FunctionComponent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from '../config/firebaseConfig';
+
+type ChildrenFunction = (username: string) => ReactNode;
+
+interface AuthWrapperProps {
+    children: ReactNode | ChildrenFunction;
+}
+
+const AuthWrapper: FunctionComponent<AuthWrapperProps> = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (authUser: User | null) => {
+            if (!authUser) {
+                navigate('/login');
+            }
+            setUser(authUser);
+        });
+
+        return () => unsubscribe();
+    }, [navigate]);
+
+    return <>{user && children}</>;
+};
+export const uname = auth.currentUser?.displayName;
+export default AuthWrapper;
