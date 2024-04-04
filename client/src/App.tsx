543997e364d791276bc815f151ed2ff6df065b06
n¿co <89131248+h0peRL@users.noreply.github.com>
2024-04-04 10:49:59 +0200

    add Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import AuthWrapper from './components/AuthWrapper';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
-  return <div className="App">Bracketingsystem</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<AuthWrapper>{<Home />}</AuthWrapper>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
