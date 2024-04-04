e5e50e5a79d2fc4b4c5de87d2887f259a9d5024c
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-04-04 08:27:03 +0200

    change setDoc to addDoc for autogenerated id

--- client/src/database/insertBracket.ts
+++ client/src/database/insertBracket.ts
@@ -1,9 +1,9 @@
-import { doc, setDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
 import IBracket from "../interfaces/IBracket";
 
 export default async function insertBracket(bracket: IBracket) {
-  return await setDoc(doc(db, "brackets", bracket.id), {
+  return await addDoc(collection(db, "brackets"), {
     title: bracket.title,
     createdAt: bracket.createdAt,
     editedAt: bracket.editedAt,
