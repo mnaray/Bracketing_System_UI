db0f54fe04b02ce1e9978fd480e83bd1bbd86d03
n¿co <nico.delvecchio@gmx.ch>
2024-04-25 10:13:23 +0200

    Add Logic of viewing each Bracket

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import { auth } from '../config/firebaseConfig';
-
+import React, { useEffect, useState } from "react";
+import { auth } from "../config/firebaseConfig";
+import getAllBracketsSnapshot from "../database/getAllBracketsSnapshot";
+import { DocumentData } from "firebase/firestore";
 
 function Home() {
-    function handleClick() {
-        auth.signOut();
+  const [brackets, setBrackets] = useState<DocumentData[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const bracketsSnapshot = await getAllBracketsSnapshot();
+        if (!bracketsSnapshot.empty) {
+          const bracketsData = bracketsSnapshot.docs.map((doc) => doc.data());
+          setBrackets(bracketsData);
+        }
+      } catch (error) {
+        console.error("Fehler beim Abrufen der Daten:", error);
+      }
     }
-    return (
-
-        <div>
-            <h1>Welcome, {auth.currentUser?.displayName}</h1>
-            <button onClick={handleClick}>Sign out</button>
-        </div>
-
-    )
+    fetchData();
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-start bg-slate-500 w-screen h-screen">
+      <div className="flex items-center flex-col mt-20">
+        <h1 className="text-xl m-2">
+          Welcome, {auth.currentUser?.displayName}
+        </h1>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => auth.signOut()}
+        >
+          Sign out
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
