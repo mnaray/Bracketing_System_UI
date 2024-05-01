a9da4dee3d9751a5c5a03b73b6f57f088543a17c
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-05-01 18:48:12 +0200

    improve representation of the bracket tree

--- client/src/components/BracketTree.tsx
+++ client/src/components/BracketTree.tsx
@@ -9,7 +9,10 @@ function BracketTree(props: IBracketTreeProps) {
     round.matches = round.matches.sort((a, b) => a.matchId - b.matchId);
     const matches = round.matches.map((match) => {
       return (
-        <li key={match.matchId} className="border border-black">
+        <li
+          key={match.matchId}
+          className="border-2 border-black my-4 mx-12 p-2 divide-y divide-blue-400 w-40 text-center text-lg"
+        >
           <p>{match.competitor1?.name || "?"}</p>
           <p>{match.competitor2?.name || "?"}</p>
         </li>
@@ -17,8 +20,8 @@ function BracketTree(props: IBracketTreeProps) {
     });
 
     return (
-      <li key={round.roundNumber} className="flex flex-col justify-center">
-        <ul className="flex flex-col">{matches}</ul>
+      <li key={round.roundNumber} className="flex flex-col justify-evenly">
+        <ul className="flex flex-col justify-evenly flex-grow">{matches}</ul>
       </li>
     );
   });
@@ -26,7 +29,7 @@ function BracketTree(props: IBracketTreeProps) {
   // add victor round
   rounds.push(<li>Winner NAME</li>);
 
-  return <ul className="flex flex-row justify-center">{rounds}</ul>;
+  return <ul className="flex flex-row justify-center h-max">{rounds}</ul>;
 }
 
 export default BracketTree;
