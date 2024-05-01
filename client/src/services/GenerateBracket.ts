3cddf62516582e0a8c6ea9e59f7de0cc809ef4a5
mnaray <m.naray.inf21@stud.bbbaden.ch>
2024-05-01 17:00:26 +0200

    finish bracket generation logic

--- client/src/services/GenerateBracket.ts
+++ client/src/services/GenerateBracket.ts
@@ -2,6 +2,7 @@ import { serverTimestamp } from "firebase/firestore";
 import IBracket from "../interfaces/IBracket";
 import ICompetitor from "../interfaces/ICompetitor";
 import IRound from "../interfaces/IRound";
+import IMatch from "../interfaces/IMatch";
 
 export default function GenerateBracket(
   uid: string,
@@ -15,14 +16,14 @@ export default function GenerateBracket(
     uid: uid,
     started: false,
     startedAt: undefined,
-    rounds: [],
+    rounds: GenerateRounds(competitorAmount),
     id: undefined,
   };
 
   return bracket;
 }
 
-function GenerateRounds(competitorAmount: number) {
+function GenerateRounds(competitorAmount: number): IRound[] {
   const competitors: ICompetitor[] = [];
 
   for (let i = 0; i < competitorAmount; i++) {
@@ -33,24 +34,42 @@ function GenerateRounds(competitorAmount: number) {
   }
 
   const rounds: IRound[] = [];
-  let round;
+  let roundCount = 1;
+  let matchCount = 1;
+
   while (competitors.length % 2 === 0) {
     const round: IRound = {
       matches: [],
-      roundNumber: 0,
+      roundNumber: roundCount,
     };
 
     for (let i = 0; i < competitors.length; i += 2) {
-      const match = {
-        competitor1: competitors[i],
-        competitor2: competitors[i + 1],
+      const match: IMatch = {
+        competitor1: roundCount === 1 ? competitors[i] : undefined,
+        competitor2: roundCount === 1 ? competitors[i + 1] : undefined,
+        matchId: matchCount,
+        started: false,
+        startedAt: undefined,
         winner: undefined,
-        matchId: undefined,
+        nextMatch: undefined,
       };
 
-      // round.matches.push(match);
+      matchCount++;
+      round.matches.push(match);
     }
 
     rounds.push(round);
+    roundCount++;
+
+    const halfCompetitors = competitors.length / 2;
+    for (let i = 0; i < halfCompetitors; i++) {
+      competitors.pop();
+    }
+
+    if (competitors.length !== 1 && competitors.length % 2 !== 0) {
+      throw Error(); //If the amount of competitors is not dividable by the base of 2, then something is wrong.
+    }
   }
+
+  return rounds;
 }
